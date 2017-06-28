07e14be764a4932f6bd7eaa1d0b84d29b4626b97
github username <github email address>
2017-06-28 14:14:15 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+  @@all = []
+  def initialize(attr_hash)
+    @name = attr_hash[:name]
+    @weight = attr_hash[:weight]
+    @height = attr_hash[:height]
+    @ships = attr_hash[:ships].map do |ship_hash|
+      Ship.new(ship_hash)
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+  def initialize(attr_hash)
+    @name = attr_hash[:name]
+    @type = attr_hash[:type]
+    @booty = attr_hash[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
